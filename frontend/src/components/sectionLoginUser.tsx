473c0b4de79b3d0934c1ad784072289b7d5b3d90
Miguel Ângelo <miguelcg958@gmail.com>
2022-08-10 13:59:08 -0300

    fix: resolves typing errors in handle click parameters

--- frontend/src/components/sectionLoginUser.tsx
+++ frontend/src/components/sectionLoginUser.tsx
@@ -1,32 +1,31 @@
-import React, { ChangeEvent, ReactNode } from "react";
+import React, { ChangeEvent } from 'react';
 
 interface LoginProps {
-  title: string,
-  type: string,
-  htmlFor: string,
+  title: string;
+  type: string;
+  htmlFor: string;
   setName: (name: string) => void;
   setEmail: (email: string) => void;
   setPassword: (password: string) => void;
 }
 
-interface EventProps {
-  event: ChangeEvent,
-  target: HTMLInputElement,
-  innerHTML: string,
-  id: string,
-}
-
-export const SectionLoginUser: React.FC<LoginProps> = ({ title, type, htmlFor, setName, setEmail, setPassword }) => {
-
-  function handleGeneric(event: ChangeEvent) {
-    switch(event.target.id) {
+export const SectionLoginUser: React.FC<LoginProps> = ({
+  title,
+  type,
+  htmlFor,
+  setName,
+  setEmail,
+  setPassword,
+}) => {
+  function handleGeneric(event: ChangeEvent<HTMLInputElement>) {
+    switch (event.target.id) {
       case 'name':
-        console.log(event.target.value)
-        return setName(event.target.value)
+        console.log(event.target.value);
+        return setName(event.target.value);
       case 'email':
-        return setEmail(event.target.value)
+        return setEmail(event.target.value);
       default:
-        return setPassword(event.target.value)
+        return setPassword(event.target.value);
     }
   }
 
@@ -35,7 +34,12 @@ export const SectionLoginUser: React.FC<LoginProps> = ({ title, type, htmlFor, s
       <label className="text-gray-900" htmlFor={htmlFor}>
         {title}
       </label>
-      <input onChange={(event: ChangeEvent) => handleGeneric(event)} id={htmlFor} className="text-gray-900 rounded p-2 text-md  border-2 border-gray-900" type={type} />
+      <input
+        id={htmlFor}
+        className="text-gray-900 rounded p-2 text-md  border-2 border-gray-900"
+        type={type}
+        onChange={handleGeneric}
+      />
     </section>
   );
 };
